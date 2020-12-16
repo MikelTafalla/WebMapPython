0324623a1988d4ce5163e655e3246f8ece7b1206
Mikel1991 <mikel362d@gmail.com>
2020-12-16 09:07:47 -0800

    recenters the map and customized info in popup markers

--- map1.py
+++ map1.py
@@ -8,12 +8,18 @@ name = list(data["NAME"])
 kind = list(data["TYPE"])
 elev = list(data["ELEV"])
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
+map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 # For loop to create multiple markers in the map
 for lt, ln, nm, kd, el in zip(lat, lon, name, kind, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=f"Name: {nm}; Type: {kd}; Elevation: {str(el)} m", icon=folium.Icon(color="green")))
+    html= f"""
+        <h4>Volcano {nm}</h4><br>
+        Type: {kd} <br>
+        Height: {str(el)}m
+        """
+    iframe = folium.IFrame(html = html, width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
