6cf16929774709cced0b2d50c5e1fa69f52282a6
Mikel1991 <mikel362d@gmail.com>
2020-12-16 08:34:27 -0800

    adds elevation to location markers

--- map1.py
+++ map1.py
@@ -6,13 +6,14 @@ lat= list(data["LAT"])
 lon= list(data["LON"])
 name = list(data["NAME"])
 kind = list(data["TYPE"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 # For loop to create multiple markers in the map
-for lt, ln, nm, kd in zip(lat, lon, name, kind):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=f"Name: {nm}; Type: {kd}", icon=folium.Icon(color="green")))
+for lt, ln, nm, kd, el in zip(lat, lon, name, kind, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=f"Name: {nm}; Type: {kd}; Elevation: {str(el)} m", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
